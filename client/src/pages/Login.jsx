37eb36899f278bcde4e421d3f60424914e3d85a7
RaviKishan24 <Ravikishankumar71@gmail.com>
2026-08-18 22:16:22 +0530

    cnf

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import { useToast } from '../context/ToastContext.jsx';
